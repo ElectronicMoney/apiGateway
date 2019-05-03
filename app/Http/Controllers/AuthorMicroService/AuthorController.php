fbb4126850a046feaca85000bbec19329f6dd7de
Emeka Augustine <cafafanscoders@gamil.com>
2019-05-03 09:53:07 -0400

    Created Controllers

---
+++ app/Http/Controllers/AuthorMicroService/AuthorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use App\Services\AuthorService;
+class AuthorController extends Controller
+{
+    use ApiResponser;
+    /**
+     * The service to consume the authors micro service
+     * @var string
+     */
+    public $authorService;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(AuthorService $authorService) {
+        $this->authorService = $authorService;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($author) {
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $author) {
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($author) {
+    }
+}
---
+++ app/Http/Controllers/BookMicroService/BookController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use App\Services\BookService;
+class BookController extends Controller
+{
+    use ApiResponser;
+    /**
+     * The service to consume the book micro service
+     * @var string
+     */
+    public $bookService;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(BookService $bookService) {
+        $this->bookService = $bookService;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($book) {
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $book) {
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($book) {
+    }
+}
