05d959a7f1c7475b64fd77be80fcd4fd67808635
Emeka Augustine <cafafanscoders@gamil.com>
2019-06-01 17:50:50 -0400

    made some file changes

--- config/services.php
+++ config/services.php
@@ -4,7 +4,4 @@ return [
     'authors' => [
         'base_uri' => env('AUTHORS_SERVICE_BASE_URL'),
     ],
-    'books' => [
-        'base_uri' => env('BOOKS_SERVICE_BASE_URL'),
-    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,18 @@
 
 
 //Authors Micro Service Routes
-$router->get('/authors', 'AuthorMicroService\AuthorController@index');
-$router->post('/authors', 'AuthorMicroService\AuthorController@store');
-$router->get('/authors/{author}', 'AuthorMicroService\AuthorController@show');
-$router->put('/authors/{author}', 'AuthorMicroService\AuthorController@update');
-$router->patch('/authors/{author}', 'AuthorMicroService\AuthorController@update');
-$router->delete('/authors/{author}', 'AuthorMicroService\AuthorController@destroy');
+$router->get('/v1/authors', 'AuthorMicroService\AuthorController@index');
+$router->post('/v1/authors', 'AuthorMicroService\AuthorController@store');
+$router->get('/v1/authors/{author}', 'AuthorMicroService\AuthorController@show');
+$router->put('/v1/authors/{author}', 'AuthorMicroService\AuthorController@update');
+$router->patch('/v1/authors/{author}', 'AuthorMicroService\AuthorController@update');
+$router->delete('/v1/authors/{author}', 'AuthorMicroService\AuthorController@destroy');
 
 //Books Micro Service Routes
-$router->get('/books', 'BookMicroService\BookController@index');
-$router->post('/books', 'BookMicroService\BookController@store');
-$router->get('/books/{book}', 'BookMicroService\BookController@show');
-$router->put('/books/{book}', 'BookMicroService\BookController@update');
-$router->patch('/books/{book}', 'BookMicroService\BookController@update');
-$router->delete('/books/{book}', 'BookMicroService\BookController@destroy');
+$router->get('/v1/books', 'BookMicroService\BookController@index');
+$router->post('/v1/books', 'BookMicroService\BookController@store');
+$router->get('/v1/books/{book}', 'BookMicroService\BookController@show');
+$router->put('/v1/books/{book}', 'BookMicroService\BookController@update');
+$router->patch('/v1/books/{book}', 'BookMicroService\BookController@update');
+$router->delete('/v1/books/{book}', 'BookMicroService\BookController@destroy');
 
